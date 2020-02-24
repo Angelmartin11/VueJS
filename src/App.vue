6406587a303a791ade09436a0094bc7ad33a89f6
Angel <angelmartin11@icloud.com>
2020-02-23 20:40:02 -0700

    To do list, se incorporo funcionalidad para agregar acciones

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,32 @@
     <h4 class="bg-primary text-white text-center p-2">
       {{name}}'s To Do List
     </h4>
-    <div class="row">
-      <div class="col font-weight-bold">Task</div>
-      <div class="col-2 font-weight-bold">Done</div>
-    </div>
-    <div class="row" v-for="t in tasks" v-bind:key="t.action">
-      <div class="col">{{t.action}}</div>
-      <div class="col-2">
-        <input type="checkbox" v-model="t.done" class="form-check-input"/>
-        {{t.done}}
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="t.done" class="form-check-input"/>
+        </div>
+      </div>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control"/>
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white" >
+          <div class=" col text-center">
+              <input type="checkbox" v-model="hideCompleted" class="form-check-input"/>
+              <label class="form-check-label font-weight-bold">
+                Hide completed tasks
+              </label>
+          </div>
       </div>
     </div>
   </div>
@@ -27,7 +44,24 @@ export default {
       tasks: [{ action: "Buy flowers", done: false},
       {action: "Practice German", done: true},
       {action: "Practice Vue", done: false},
-      {action: "Work out", done: false}]
+      {action: "Work out", done: false}],
+      hideCompleted: true,
+      newItemText: ""
+    }
+  },
+  computed:{
+    filteredTasks(){
+      return this.hideCompleted ?
+      this.tasks.filter(t=> !t.done) : this.tasks
+    }
+  },
+  methods: {
+    addNewTodo(){
+      this.tasks.push({
+        action: this.newItemText,
+        done: false
+      });
+      this.newItemText ="";
     }
   }
 }
