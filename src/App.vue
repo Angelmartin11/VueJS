8dd6c4f2d5f69011a759546e1ec6223a01044150
Angel <angelmartin11@icloud.com>
2020-02-26 21:16:51 -0700

    Se agrego la funcionalidad de guardado en memoria del navegador, y ahora se pueden eliminar tasks

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,23 @@
       {{name}}'s To Do List
     </h4>
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Task</div>
-        <div class="col-2 font-weight-bold">Done</div>
-      </div>
-      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-        <div class="col">{{t.action}}</div>
-        <div class="col-2 text-center">
-          <input type="checkbox" v-model="t.done" class="form-check-input"/>
+      <div class="row" v-if="filteredTasks.length == 0">
+        <div class="col text-center">
+          <b>Nothing to do...</b>
         </div>
       </div>
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+          <div class="col">{{t.action}}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="t.done" class="form-check-input"/>
+          </div>
+        </div>
+      </template>
       <div class="row py-2">
         <div class="col">
           <input v-model="newItemText" class="form-control"/>
@@ -29,6 +36,11 @@
                 Hide completed tasks
               </label>
           </div>
+          <div class="col text-center">
+            <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">
+              Delete Completed
+            </button>
+          </div>
       </div>
     </div>
   </div>
@@ -41,10 +53,7 @@ export default {
   data(){
     return{
       name: "Martin",
-      tasks: [{ action: "Buy flowers", done: false},
-      {action: "Practice German", done: true},
-      {action: "Practice Vue", done: false},
-      {action: "Work out", done: false}],
+      tasks: [],
       hideCompleted: true,
       newItemText: ""
     }
@@ -61,7 +70,21 @@ export default {
         action: this.newItemText,
         done: false
       });
+      this.storeData();
       this.newItemText ="";
+    },
+    storeData(){
+         localStorage.setItem("todos", JSON.stringify(this.tasks));
+    },
+    deleteCompleted(){
+      this.tasks = this.tasks.filter(t => !t.done);
+      this.storeData();
+    }
+  },
+  created(){
+    let data = localStorage.getItem("todos");
+    if(data!=null){
+      this.tasks = JSON.parse(data);
     }
   }
 }
